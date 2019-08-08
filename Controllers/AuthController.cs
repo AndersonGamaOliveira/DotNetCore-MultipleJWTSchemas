439c0bd0a8818aa609604379aa61028501ee4c70
Anderson Oliveira <anderson.oliveira@solutis.com.br>
2019-08-08 16:28:06 -0300

    first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,93 @@
+﻿using DotNetCore_MultipleJWTSchemas.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace DotNetCore_MultipleJWTSchemas.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SigningConfigurations _signingConfigurations;
+
+        public AuthController(SigningConfigurations signingConfigurations)
+        {
+            _signingConfigurations = signingConfigurations;
+        }
+
+        #region Token 1
+
+        [HttpGet("authToken1")]
+        [AllowAnonymous]
+        public ActionResult AuthToken1()
+        {
+            return Ok(gerarToken1());
+        }
+
+        private object gerarToken1()
+        {
+            DateTime dataCriacao = DateTime.Now;
+            DateTime dataExpiracao = dataCriacao + TimeSpan.FromSeconds(300);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = "IssuerToken1",
+                Audience = "AudienceToken1",
+                SigningCredentials = _signingConfigurations.SigningCredentials,
+                NotBefore = dataCriacao,
+                Expires = dataExpiracao
+            });
+
+            return handler.WriteToken(securityToken);
+        }
+
+        [HttpGet("getToken1")]
+        [Authorize("PolicyToken1")]
+        public ActionResult<string> GetToken1()
+        {
+            return "OK";
+        }
+
+        #endregion Token 1
+
+        #region Token 2
+
+        [HttpGet("authToken2")]
+        [AllowAnonymous]
+        public ActionResult AuthToken2()
+        {
+            return Ok(gerarToken2());
+        }
+
+        private object gerarToken2()
+        {
+            DateTime dataCriacao = DateTime.Now;
+            DateTime dataExpiracao = dataCriacao + TimeSpan.FromSeconds(300);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = "IssuerToken2",
+                Audience = "AudienceToken2",
+                SigningCredentials = _signingConfigurations.SigningCredentials,
+                NotBefore = dataCriacao,
+                Expires = dataExpiracao
+            });
+
+            return handler.WriteToken(securityToken);
+        }
+
+        [HttpGet("getToken2")]
+        [Authorize("PolicyToken2")]
+        public ActionResult<string> GetToken2()
+        {
+            return "OK";
+        }
+
+        #endregion Token 2
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,85 @@
+﻿using DotNetCore_MultipleJWTSchemas.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DotNetCore_MultipleJWTSchemas
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var signingConfigurations = new SigningConfigurations();
+            services.AddSingleton(signingConfigurations);
+
+            services.AddAuthentication()
+                .AddJwtBearer("BearerToken1", bearerOptions =>
+                {
+                    bearerOptions.TokenValidationParameters.IssuerSigningKey = signingConfigurations.Key;
+                    bearerOptions.TokenValidationParameters.ValidateAudience = true;
+                    bearerOptions.TokenValidationParameters.ValidAudience = "AudienceToken1";
+                    bearerOptions.TokenValidationParameters.ValidateIssuer = true;
+                    bearerOptions.TokenValidationParameters.ValidIssuer = "IssuerToken1";
+                    bearerOptions.TokenValidationParameters.ValidateIssuerSigningKey = true;
+                    bearerOptions.TokenValidationParameters.ValidateLifetime = true;
+                    bearerOptions.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
+                })
+                .AddJwtBearer("BearerToken2", bearerOptions =>
+                {
+                    bearerOptions.TokenValidationParameters.IssuerSigningKey = signingConfigurations.Key;
+                    bearerOptions.TokenValidationParameters.ValidateAudience = true;
+                    bearerOptions.TokenValidationParameters.ValidAudience = "AudienceToken2";
+                    bearerOptions.TokenValidationParameters.ValidateIssuer = true;
+                    bearerOptions.TokenValidationParameters.ValidIssuer = "IssuerToken2";
+                    bearerOptions.TokenValidationParameters.ValidateIssuerSigningKey = true;
+                    bearerOptions.TokenValidationParameters.ValidateLifetime = true;
+                    bearerOptions.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
+                });
+
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("PolicyToken1", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes("BearerToken1")
+                    .RequireAuthenticatedUser()
+                    .Build());
+
+                auth.AddPolicy("PolicyToken2", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes("BearerToken2")
+                    .RequireAuthenticatedUser()
+                    .Build());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
